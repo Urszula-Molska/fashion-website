dd48f120b13c4ebf16c70d966aedc37381b51f23
Urszula Molska <urszula_molska@wp.pl>
2023-12-07 00:23:13 +0100

    lower size of image, prettier-plugin-tailwindcss

--- src/components/BestSelling.jsx
+++ src/components/BestSelling.jsx
@@ -1,90 +1,90 @@
-import { useState, useEffect } from "react";
-import { Notify } from "notiflix/build/notiflix-notify-aio";
-import Loader from "../components/Loader/Loader.jsx";
-import sprite from "../assets/sprite.svg";
-import axios from "axios";
+import { useState, useEffect } from 'react';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import Loader from '../components/Loader/Loader.jsx';
+import sprite from '../assets/sprite.svg';
+import axios from 'axios';
 
 const BestSelling = () => {
-  const [pictures, setPictures] = useState([]);
-  const [limit, setLimit] = useState(3);
-  const [isLoading, setLoading] = useState(false);
+	const [pictures, setPictures] = useState([]);
+	const [limit, setLimit] = useState(3);
+	const [isLoading, setLoading] = useState(false);
 
-  const fetchPictures = async (limit) => {
-    const URL = `https://fakestoreapi.com/products?limit=${limit}`;
-    try {
-      if (limit < 21) {
-        setLoading(true);
-        const response = await axios.get(`${URL}`);
-        setPictures(response.data);
-        setLoading(false);
-      } else {
-        Notify.info("There are no more records to display!");
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
+	const fetchPictures = async limit => {
+		const URL = `https://fakestoreapi.com/products?limit=${limit}`;
+		try {
+			if (limit < 21) {
+				setLoading(true);
+				const response = await axios.get(`${URL}`);
+				setPictures(response.data);
+				setLoading(false);
+			} else {
+				Notify.info('There are no more records to display!');
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
-  useEffect(() => {
-    fetchPictures(limit);
-  }, [limit]);
+	useEffect(() => {
+		fetchPictures(limit);
+	}, [limit]);
 
-  return (
-    <>
-      <div className="bg-white pt-20  w-full px-[10px] 2xl:pt-[114px] flex flex-col  justify-center items-center pb-[99px] ">
-        <h2 className="font-roboto_slab font-medium text-[36px] tab&2xl:text-[64px] text-green800 text-center">
-          Best selling
-        </h2>
-        <p className="font-poppins font-medium text-[22px] text-green800 text-center pt-[37px] pb-10 2xl:pb-20">
-          Get in on the trend with our curated selection of best-selling styles.
-        </p>
-        <ul className="flex flex-wrap gap-[43px] mb-20 justify-center">
-          {pictures.map((picture) => (
-            <li
-              key={picture.id}
-              className="w-full max-w-[430px] flex items-center flex-col"
-            >
-              <div className="tab&2xl:w-[430px] tab&2xl:h-[566px] mb-3 tab&2xl:mb-[51px] w-full">
-                {isLoading === true ? (
-                  <Loader />
-                ) : (
-                  <img
-                    className="tab&2xl:w-[430px] tab&2xl:h-[566px] object-contain block bg-white w-full"
-                    src={picture.image}
-                    alt={picture.title}
-                  ></img>
-                )}
-              </div>
-              <p className="font-poppins text-[22px] text-gray400 font-semibold text-center mb-2 w-[303px]">
-                {picture.title}
-              </p>
-              <div className="font-poppins text-[22px] text-gray200 font-medium flex items-center justify-center  ">
-                <p className="mr-[39px]">${picture.price}</p>
-                <div className="h-[27px] mr-[48px] border-[1px] text-[22px] text-gray200 font-medium"></div>
-                <p className="mr-[9px]">{picture.rating.rate}</p>
-                <svg className="w-6 h-6">
-                  <use href={`${sprite}#icon-star`}></use>
-                </svg>
-              </div>
-            </li>
-          ))}
-        </ul>
+	return (
+		<>
+			<div className="flex w-full flex-col items-center justify-center bg-white px-[10px]  pb-[99px] pt-20 2xl:pt-[114px] ">
+				<h2 className="text-center font-roboto_slab text-[36px] font-medium text-green800 tab&2xl:text-[64px]">
+					Best selling
+				</h2>
+				<p className="pb-10 pt-[37px] text-center font-poppins text-[22px] font-medium text-green800 2xl:pb-20">
+					Get in on the trend with our curated selection of best-selling styles.
+				</p>
+				<ul className="mb-20 flex flex-wrap justify-center gap-[43px]">
+					{pictures.map(picture => (
+						<li
+							key={picture.id}
+							className="flex w-full max-w-[430px] flex-col items-center"
+						>
+							<div className="mb-3 w-full tab&2xl:mb-[51px] tab&2xl:h-[566px] tab&2xl:w-[430px]">
+								{isLoading === true ? (
+									<Loader />
+								) : (
+									<img
+										className="block w-full bg-white object-contain tab&2xl:h-[566px] tab&2xl:w-[430px]"
+										src={picture.image}
+										alt={picture.title}
+									></img>
+								)}
+							</div>
+							<p className="mb-2 w-[303px] text-center font-poppins text-[22px] font-semibold text-gray400">
+								{picture.title}
+							</p>
+							<div className="flex items-center justify-center font-poppins text-[22px] font-medium text-gray200  ">
+								<p className="mr-[39px]">${picture.price}</p>
+								<div className="mr-[48px] h-[27px] border-[1px] text-[22px] font-medium text-gray200"></div>
+								<p className="mr-[9px]">{picture.rating.rate}</p>
+								<svg className="h-6 w-6">
+									<use href={`${sprite}#icon-star`}></use>
+								</svg>
+							</div>
+						</li>
+					))}
+				</ul>
 
-        {limit < 21 && (
-          <button
-            id="fetch"
-            onClick={() => setLimit(limit + 6)}
-            className="flex items-center pl-[55px] relative w-[224px] h-[65px] border-[2px] rounded-[3px] border-green800 font-poppins text-[22px] font-medium text-green800"
-          >
-            See all
-            <svg className="w-[23px] h-[23px] absolute right-[55px] top-[20px] z-10 fill-green800 animate-pulse">
-              {<use href={`${sprite}#icon-arrow-right`}></use>}
-            </svg>
-          </button>
-        )}
-      </div>
-    </>
-  );
+				{limit < 21 && (
+					<button
+						id="fetch"
+						onClick={() => setLimit(limit + 6)}
+						className="relative flex h-[65px] w-[224px] items-center rounded-[3px] border-[2px] border-green800 pl-[55px] font-poppins text-[22px] font-medium text-green800"
+					>
+						See all
+						<svg className="absolute right-[55px] top-[20px] z-10 h-[23px] w-[23px] animate-pulse fill-green800">
+							{<use href={`${sprite}#icon-arrow-right`}></use>}
+						</svg>
+					</button>
+				)}
+			</div>
+		</>
+	);
 };
 
 export default BestSelling;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,58 +1,58 @@
-import image from "../assets/pexels-dima-valkov.png";
-import sprite from "../assets/sprite.svg";
+import image from '../assets/pexels-dima-valkov.png';
+import sprite from '../assets/sprite.svg';
 const Hero = () => {
-  return (
-    <>
-      <div className="flex flex-wrap tab:flex-col tab: items-center justify-center 2xl:flex-nowrap pb-[80px] 2xl:pb-[166px] mx-auto 2xl:w-[1377px] w-full">
-        <div className="flex flex-col tab:relative mob:place-items-center mob:w-full">
-          <div className="w-full h-auto tab&2xl:w-[649px] mb-[27px] tab&2xl:mb-[27px] mob:whitespace-normal mob:max-w-[340px] mob:px-5">
-            <h1 className="text-green800 font-rufina text-[46px] tab&2xl:text-[86px] font-bold leading-tight ">
-              Discover and Find Your Own Fashion!
-            </h1>
-          </div>
-          <div className="tab&2xl:w-[565px] mob:max-w-[340px] mob:px-5">
-            <p className="text-green600 font-poppins font-medium text-[20px] tab&2xl:text-2xl leading-[220%] tab:mb-10">
-              Explore our curated collection of stylish clothing and accessories
-              tailored to your unique taste.
-            </p>
-          </div>
-          <button
-            className=" mob:px-6 py-3 mob:mb-5 mob:text-[18px] tab:absolute tab:z-20 tab:top-[500px] tab:left-[-30px]  cursor-pointer mt-[30px] tab&2xl:mt-[54px] blockfont-poppins text-[20px]
-             text-white font-medium text-center tab&2xl:w-[258px] tab&2xl:h-[74px] bg-green800 rounded-[3px] 
-            drop-shadow-buttonShadow"
-          >
-            EXPLORE NOW
-          </button>
-        </div>
-        <div
-          className=" relative  tab&2xl:w-[570px]  tab&2xl:h-[717px] shrink-0 bg-green400 rounded-tl-[141px] rounded-tr-[45px] rounded-bl-[82px] 
-        rounded-br-[150px] 2xl:ml-[160px] mob:max-w-[570px] w-full"
-        >
-          <img
-            src={image}
-            alt="lady in a green jacket"
-            className=" shrink-0 tab&2xl:w-[570px] tab&2xl:h-[717px] mob:max-w-full "
-          ></img>
-          <div
-            className="absolute right-0 top-[155px] z-10 w-1/5 h-auto max-h-[110px] max-w-[119px]"
-            style={{ right: "3%", top: "22%" }}
-          >
-            <svg className="w-full ">
-              <use href={`${sprite}#icon-dots`}></use>
-            </svg>
-          </div>
-          <div
-            className="absolute  z-10 w-1/5 h-auto mob:max-w-full max-h-[110px] max-w-[119px]"
-            style={{ left: "4%", bottom: "12%" }}
-          >
-            <svg className="w-full">
-              <use href={`${sprite}#icon-dots`}></use>
-            </svg>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="tab: mx-auto flex w-full flex-wrap items-center justify-center pb-[80px] 2xl:w-[1377px] 2xl:flex-nowrap 2xl:pb-[166px] tab:flex-col">
+				<div className="flex flex-col tab:relative mob:w-full mob:place-items-center">
+					<div className="mb-[27px] h-auto w-full tab&2xl:mb-[27px] tab&2xl:w-[649px] mob:max-w-[340px] mob:whitespace-normal mob:px-5">
+						<h1 className="font-rufina text-[46px] font-bold leading-tight text-green800 tab&2xl:text-[86px] ">
+							Discover and Find Your Own Fashion!
+						</h1>
+					</div>
+					<div className="tab&2xl:w-[565px] mob:max-w-[340px] mob:px-5">
+						<p className="font-poppins text-[20px] font-medium leading-[220%] text-green600 tab&2xl:text-2xl tab:mb-10">
+							Explore our curated collection of stylish clothing and accessories
+							tailored to your unique taste.
+						</p>
+					</div>
+					<button
+						className="blockfont-poppins mt-[30px] cursor-pointer rounded-[3px] bg-green800 py-3 text-center text-[20px]  font-medium text-white drop-shadow-buttonShadow tab&2xl:mt-[54px] tab&2xl:h-[74px]
+             tab&2xl:w-[258px] tab:absolute tab:left-[-30px] tab:top-[500px] tab:z-20 mob:mb-5 mob:px-6 
+            mob:text-[18px]"
+					>
+						EXPLORE NOW
+					</button>
+				</div>
+				<div
+					className="relative w-full shrink-0 rounded-bl-[82px] rounded-br-[150px] rounded-tl-[141px] rounded-tr-[45px] 
+        bg-green400 2xl:ml-[160px] tab&2xl:h-[717px] tab&2xl:w-[570px] mob:max-w-[570px]"
+				>
+					<img
+						src={image}
+						alt="lady in a green jacket"
+						className="shrink-0 tab&2xl:h-[717px] tab&2xl:w-[570px] mob:max-w-full "
+					></img>
+					<div
+						className="absolute right-0 top-[155px] z-10 h-auto max-h-[110px] w-1/5 max-w-[119px]"
+						style={{ right: '3%', top: '22%' }}
+					>
+						<svg className="w-full ">
+							<use href={`${sprite}#icon-dots`}></use>
+						</svg>
+					</div>
+					<div
+						className="absolute z-10 h-auto max-h-[110px] w-1/5 max-w-[119px] mob:max-w-full"
+						style={{ left: '4%', bottom: '12%' }}
+					>
+						<svg className="w-full">
+							<use href={`${sprite}#icon-dots`}></use>
+						</svg>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Hero;
